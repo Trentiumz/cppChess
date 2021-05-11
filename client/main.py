27553140b71011e83a3f96618f02408d22569ad8
Daniel Ye <danielyeqt@gmail.com>
2021-05-11 10:12:47 -0400

    Add Images & Sound

--- client/main.py
+++ client/main.py
@@ -7,9 +7,14 @@ from runner import *
 class ChessApplication(Application):
     x = 0
     y = 250
+    bishop = Image("./assets/sprites/bking.png", 50, 50)
+    music = Sound("./assets/menu/StartMusic.wav")
 
     def __init__(self, width, height):
         super().__init__(width, height)
+
+    def setup(self):
+        self.music.play()
 
     def mouse_pressed(self, event):
         print(event.button)
@@ -17,10 +22,9 @@ class ChessApplication(Application):
     def draw(self):
         background(255, 0, 0)
         self.x += 30 * time_delta
-        fill(0, 0, 0)
-        rect(self.x, self.y, 30, 30)
         fill(0, 0, 255)
-        circle(self.x, self.y, 30)
+        rect(self.x, self.y, 50, 50)
+        image(self.bishop, self.x, self.y, 100)
 
 
 if __name__ == "__main__":
--- client/runner.py
+++ client/runner.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 import sys
 
 sleeper = pygame.time.Clock()
@@ -15,16 +16,42 @@ cur_fill = (0, 0, 0)
 fps = 30
 time_delta = 1 / fps
 
+looping = []
+
 
 class Image:
     surface = None
 
     def __init__(self, path, width, height):
-        self.surface = pygame.image.load(path)
-        pygame.transform.scale(self.surface, (width, height))
+        self.surface = pygame.transform.scale(pygame.image.load(path), (width, height))
 
     def set_alpha(self, alpha):
         self.surface.set_alpha(alpha)
+
+
+class Sound:
+    cur_sound = None
+    last_start = 0
+
+    def __init__(self, path):
+        if not pygame.mixer.get_init():
+            pygame.mixer.init()
+        self.cur_sound = pygame.mixer.Sound(path)
+
+    def play(self, loop=False):
+        self.cur_sound.play()
+        self.last_start = time.time()
+        if loop and self not in looping:
+            looping.append(self)
+
+    def stop(self, fadeout_time=0):
+        self.cur_sound.fadeout(fadeout_time)
+        self.last_start = 0
+
+        looping.remove(self)
+
+    def playing(self):
+        return time.time() - self.last_start > self.cur_sound.get_length()
 
 
 class Application:
@@ -44,6 +71,7 @@ class Application:
         else:
             main_application = self
 
+        self.setup()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -53,12 +81,20 @@ class Application:
                     self.mouse_pressed(event)
                 elif event.type == pygame.MOUSEBUTTONUP:
                     self.mouse_released(event)
+
+            for sound in looping:
+                if not sound.playing():
+                    sound.play()
+
             mouse_x = pygame.mouse.get_pos()[0]
             mouse_y = pygame.mouse.get_pos()[1]
             self.draw()
             pygame.display.update()
 
             sleeper.tick(fps)
+
+    def setup(self):
+        pass
 
     def draw(self):
         pass
@@ -104,4 +140,4 @@ def fill(r, g, b):
 
 def image(to_add: Image, x, y, alpha=255):
     to_add.set_alpha(alpha)
-    main_application.screen.blit(to_add, (x, y))
+    main_application.screen.blit(to_add.surface, (x, y))
